74b530b3073f9c239fdc6a7826410eb9505a587e
Bergan Oudshoorn <Bergan00@gmail.com>
2021-04-21 10:33:45 -0400

    working form

---
+++ scripts/form-submit.js
@@ -0,0 +1,20 @@
+var form = document.getElementById("my-form");
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
