659ef6b4c0aceaecdfe9ed621ab27337204f5fdc
Bergan Oudshoorn <Bergan00@gmail.com>
2021-04-22 19:57:03 -0400

    window transitions

--- scripts/window-loader.js
+++ scripts/window-loader.js
@@ -3,5 +3,9 @@ document.addEventListener('DOMContentLoaded', event => {
     $(window).on("load",function(){
         $(".loader-wrapper").fadeOut("slow");
     });
-    
+
+    $(document).ready(function(){
+        $(window).scrollTop(0);
+    });
+
 })
