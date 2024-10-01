2fc5e80ff35ee7f3d91f6ffa9b54f1842569db10
Bergan Oudshoorn <73203015+Bergan404@users.noreply.github.com>
2024-10-01 14:46:28 -0400

    Fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bergan404.github.io',
-    base: 'my-repo',
+    site: 'https://bergan404.github.io'
   })
